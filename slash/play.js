ce2eaed6d9e819142a6f749946851eefe1c0c5ca
DuckSpa <71780252+DuckSpa@users.noreply.github.com>
2022-10-04 13:51:29 +0000

    Better play.js with progress bar
    added features like progress bar for the song that is currently being played

--- slash/play.js
+++ slash/play.js
@@ -1,97 +1,152 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require("discord.js");
-const { QueryType } = require("discord-player");
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed} = require("discord.js")
+const { QueryType } = require("discord-player")
 
 module.exports = {
-    data: new SlashCommandBuilder()
-    .setName("play")
-    .setDescription("plays a song")
-    .addSubcommand((subcommand) => 
-    subcommand.setName("song")
-    .setDescription("plays a single song from url")
-    .addStringOption((option) => option.setName("url").setDescription("the song url").setRequired(true))
-    )
-    .addSubcommand((subcommand) => 
-    subcommand.setName("playlist")
-    .setDescription("Loads a playlist")
-    .addStringOption((option) => option.setName("url").setDescription("playlist url").setRequired(true))
-    )
-    .addSubcommand((subcommand) =>
-    subcommand.setName("search")
-    .setDescription("Search song by keywords")
-    .addStringOption((option) => option.setName("searchterms").setRequired(true))),
+	data: new SlashCommandBuilder()
+		.setName("play")
+		.setDescription("Reproduce canciones de youtube")
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("song")
+				.setDescription("Reproduce una sola cancion desde un URL")
+				.addStringOption((option) => option.setName("url").setDescription("URL de la cancion").setRequired(true))
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("playlist")
+				.setDescription("Reproduce una lista de canciones desde un URL")
+				.addStringOption((option) => option.setName("url").setDescription("URL de la lista de canciones").setRequired(true))
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("search")
+				.setDescription("Busca una cancion en base a palabras claves")
+				.addStringOption((option) =>
+					option.setName("searchterms").setDescription("Palabras claves a buscar").setRequired(true)
+				)
+		),
+	run: async ({ client, interaction}) => {
+		if (!interaction.member.voice.channel) return interaction.editReply("Necesitas estar un chat de voz para usar este comando!")
 
-    run: async({ client, interaction}) => {
-        if(!interaction.member.voice.channel){
-            return interaction.editReply("Necesitas estar en un chat de voz para usar el comando!");
-        }
+		const queue = await client.player.createQueue(interaction.guild)
+		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
 
-        const queue = await client.player.createQueue(interactoin.guild);
-        if(!queue.connection) await queue.connect(interaction.member.voice.channel)
+		let embed = new MessageEmbed()
 
-        let embed = new MessageEmbed()
-
-        if(interaction.options.getSubcommand === "song"){
-            let url = interaction.options.getString("url");
+		if (interaction.options.getSubcommand() === "song") {
+            let url = interaction.options.getString("url")
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
                 searchEngine: QueryType.YOUTUBE_VIDEO
             })
-
-            if(result.tracks.length === 0){
-               return interaction.editReply("No hay resultados!")
+            
+            if (result.tracks.length === 0){
+                return interaction.editReply({
+                    embeds: [new MessageEmbed()
+                        .setDescription(`:x:Sin resultados!`)
+                        .setColor('#b2a89e')
+                    ]
+                })
             }
 
-            const song = result.tracks[0];
-            await queue.addTrack(song);
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+
+            let bar = queue.createProgressBar({
+                queue: false,
+                length: 20,
+            })
+
+            const options = interaction.options._hoistedOptions;
+            const user = (options.find((e) => e.name === "user") && options.find((e) => e.name === "user").member.user) || interaction.user;
+            const image = user.displayAvatarURL()
+            const currentSong = queue.current
+
             embed
-            .setDescription(`**[${song.title}](${song.url})** se ha agregado a la cola`)
-            .setThumbnail(song.thumbnail)
-            .setFooter( text `Duracion: ${song.duration}`)
-        }
-        
-        else if(interaction.options.getSubcommand === "playlist"){
-            let url = interaction.options.getString("url");
+                .setDescription(`**[${song.title}](${song.url})** \n añadido a la lista de reproduccion!`)
+                .setThumbnail(song.thumbnail)
+                .addFields(
+                    { name: `:musical_note: Reproduciendo: ${currentSong.title} :musical_note:`, value: `${bar}` }
+                )
+                .setFooter({ text: `Duracion: ${currentSong.duration}`, iconURL: image})
+                .setTimestamp()
+                .setColor('#b2a89e')
+            
+		} else if (interaction.options.getSubcommand() === "playlist") {
+            let url = interaction.options.getString("url")
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
                 searchEngine: QueryType.YOUTUBE_PLAYLIST
             })
 
-            if(result.tracks.length === 0){
-               return interaction.editReply("No hay resultados!")
+            if (result.tracks.length === 0){
+                return interaction.editReply({
+                    embeds: [new MessageEmbed()
+                        .setDescription(`:x:Sin resultados!`)
+                        .setColor('#b2a89e')
+                    ]
+                })
             }
-
+            
             const playlist = result.playlist
-            await queue.addTracks(result.tracks);
-            embed
-            .setDescription(`**[${playlist.title}](${playlist.url})** se ha agregado a la cola`)
-            .setThumbnail(playlist.thumbnail)
-            .setFooter( text `Duracion: ${playlist.duration}`)
-        }
+            await queue.addTracks(result.tracks)
         
-        else if(interaction.options.getSubcommand === "search"){
-            let url = interaction.options.getString("url");
+            const options = interaction.options._hoistedOptions;
+            const user = (options.find((e) => e.name === "user") && options.find((e) => e.name === "user").member.user) || interaction.user;
+            const image = user.displayAvatarURL()
+
+            embed
+                .setDescription(`**${result.tracks.length} Las canciones de: [${playlist.title}](${playlist.url})** han sido añadidas a la lista`)
+                .setThumbnail(playlist.thumbnail)
+                .setFooter({iconURL: image})
+                .setTimestamp()
+                .setColor('#b2a89e')
+            
+		} else if (interaction.options.getSubcommand() === "search") {
+            let url = interaction.options.getString("searchterms")
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
                 searchEngine: QueryType.AUTO
             })
 
-            if(result.tracks.length === 0){
-               return interaction.editReply("No hay resultados!")
+            if (result.tracks.length === 0){
+                return interaction.editReply({
+                    embeds: [new MessageEmbed()
+                        .setDescription(`:x:Sin resultados!`)
+                        .setColor('#b2a89e')
+                    ]
+                })
             }
-
+                
             const song = result.tracks[0]
-            await queue.addTrack(song);
-            embed
-            .setDescription(`**[${song.title}](${song.url})** se ha agregado a la cola`)
-            .setThumbnail(song.thumbnail)
-            .setFooter( text `Duracion: ${song.duration}`)
-        }
+            await queue.addTrack(song)
+            
+            let bar = queue.createProgressBar({
+                queue: false,
+                length: 20,
+            })
 
-        if(!queue.playing) await queue.play();
+            const options = interaction.options._hoistedOptions;
+            const user = (options.find((e) => e.name === "user") && options.find((e) => e.name === "user").member.user) || interaction.user;
+            const currentSong = queue.current
+            const image = user.displayAvatarURL()
+
+            embed
+                .setDescription(`**[${song.title}](${song.url})** \n añadido a la lista de reproduccion!`)
+                .setThumbnail(song.thumbnail)
+                .addFields(
+                    { name: `:musical_note: Reproduciendo: ${currentSong.title} :musical_note:`, value: `${bar}` }
+                )
+                .setFooter({ text: `Duracion: ${currentSong.duration}`, iconURL: image})
+                .setTimestamp()
+                .setColor('#b2a89e')
+            
+		}
+        if (!queue.playing) await queue.play()
         await interaction.editReply({
             embeds: [embed]
-        })
-    }
 
+        })
+	}
 }
